01bb68bb82feb0cd2866c5a237f1367d34b3b729
Michael Houmann <michael@michael-h.dk>
2017-11-16 17:01:52 +0100

    Added delete token to User

--- server/models/user.js
+++ server/models/user.js
@@ -113,6 +113,17 @@ UserSchema.pre('save', function(next) {
 
  });
 
+UserSchema.methods.removeToken = function (token) {
+
+  let user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 let User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
