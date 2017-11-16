aed86f4c248c1c73cba4ad562d4d10a2d20e7639
Michael Houmann <michael@michael-h.dk>
2017-11-16 14:28:30 +0100

    Added test for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -224,13 +224,13 @@ describe('POST /users', () => {
           expect(user).toExist();
           expect(user.password).toNotBe(password);
           done();
-        });
+        }).catch((e) => done(e));
       });
   });
 
   it('should return validation errors if request invalid', (done) => {
     let email = 'testing.com';
-    let password = '1';
+    let password = '1234';
 
     request(app)
       .post('/users')
@@ -250,5 +250,60 @@ describe('POST /users', () => {
       .end(done);
   });
 
-
 });//(POST /users)
+
+describe('POST /users/login', () => {
+
+  it('should log login and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'kode1234'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+
+      });
+  });
+
+});
